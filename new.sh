5a5152ccfab7e095c667a26f3f3b8a924e20fa6b
Sven Schoradt <sven.schoradt@googlemail.com>
2020-03-31 09:07:54 +0200

    ADD: script to create new katas

---
+++ new.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER=$1
+
+git archive --format tar --output /tmp/kata-template.tar HEAD
+
+mkdir -p $FOLDER
+
+cd $FOLDER
+
+tar -xf /tmp/kata-template.tar
+
+rm -f new.sh
+
+git init
+git add .
+git commit -m 'SYS: start with kata template'
+
+rm /tmp/kata-template.tar
